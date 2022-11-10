4e2f52d19044d4e1c11fb0d00319cf4f9b93e819
Arthur Moura <110835670+Arthur-Moura-Cardoso@users.noreply.github.com>
2022-11-10 01:00:39 -0300

    Adicionando responsividade

--- main.js
+++ main.js
@@ -1,44 +1,11 @@
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+const btnMobile = document.getElementById('btn-mobile');
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefaul()
 
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
-            link.style.animation 
-            ? (link.style.animation = "") 
-            : (link.style.animation = `navLinkFade 0.5 ease forwards 0.3s`);
-        })
-    }
-
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass)
-        this.animateLinks();
-    }
-
-
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
-    }
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
 }
 
-const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-);
-
-mobileNavbar.init();
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
