188446c344d3ac371ccb9db7f71b5592dd863767
Arthur Moura <110835670+Arthur-Moura-Cardoso@users.noreply.github.com>
2022-11-20 22:06:37 -0300

    Atualizando NavBar mobile

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const btnMobile = document.getElementById('btn-mobile');
+const menuLink = document.querySelectorAll('.link')
+
+menuLink.forEach(link =>{
+    link.addEventListener('click', fecharMenu);
+    link.addEventListener('touchstart', fecharMenu);
+})
+
 
 function toggleMenu(event) {
     if(event.type === 'touchstart') event.preventDefaul()
 
     const nav = document.querySelector('nav');
     nav.classList.toggle('active');
+}
+
+function fecharMenu(event) {
+    if(event.type === 'touchstart') event.preventDefaul()
+
+    const nav = document.querySelector('nav');
+    nav.classList.remove('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
